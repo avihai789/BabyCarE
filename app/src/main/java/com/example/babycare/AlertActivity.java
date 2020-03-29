29993b7036b84bac92cbd2d4da30d5d71516524c
Avihai Zeligman <avihaize@std.hac.ac.il>
2020-03-29 22:22:42 +0300

    when clicking on alert the date and time of the alert is added to history.(bug: only adds one)

--- app/src/main/java/com/example/babycare/AlertActivity.java
+++ app/src/main/java/com/example/babycare/AlertActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,23 +13,26 @@ import java.util.List;
 
 public class AlertActivity extends AppCompatActivity {
 
-    ListView listview;
-
-    String[] ListElements = new String[] {
-            "Sunday",
-            "Monday",
-    };
+    String passedVar=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alert);
 
-        listview = findViewById(R.id.alerts_dates_list);
-
-        final List<String> ListElementsArrayList = new ArrayList<>(Arrays.asList(ListElements));
+        ListView listview1 = findViewById(R.id.alerts_dates_list1);
+        final List<String> ListElementsArrayList = new ArrayList<String>(Arrays.<String>asList());
         final ArrayAdapter<String> adapter = new ArrayAdapter<>
                 (AlertActivity.this, android.R.layout.simple_list_item_1, ListElementsArrayList);
-        listview.setAdapter(adapter);
+        listview1.setAdapter(adapter);
+
+        if(MainActivity.DATE!=null) {
+            passedVar = getIntent().getStringExtra(MainActivity.DATE);
+            ListElementsArrayList.add(MainActivity.DATE);
+            adapter.notifyDataSetChanged();
+        }
+
+
+
     }
 }
--- app/src/main/java/com/example/babycare/MainActivity.java
+++ app/src/main/java/com/example/babycare/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.babycare;
 
+
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,19 +11,34 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
-import android.widget.TextView;
+import android.widget.ListView;
+
+import java.util.GregorianCalendar;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private ImageButton last_alerts_button;
-    private ImageButton alert;
+    private ImageButton alert_button;
+    public static String DATE = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        alert = findViewById(R.id.alert_btn);
+        ListView listview2 = findViewById(R.id.alerts_dates_list2);
+        final List<String> ListElementsArrayList = new ArrayList<String>(Arrays.<String>asList());
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>
+                (MainActivity.this, android.R.layout.simple_list_item_1, ListElementsArrayList);
+        listview2.setAdapter(adapter);
+
+        alert_button = findViewById(R.id.alert_btn);
         last_alerts_button = findViewById(R.id.last_alert_btn);
 
         last_alerts_button.setOnClickListener(new View.OnClickListener() {
@@ -31,19 +47,12 @@ public class MainActivity extends AppCompatActivity {
                 openlastalertsactivity();
             }
         });
-
-        alert.setOnClickListener(new View.OnClickListener() {
-
-            //public void onClick(View v) {
-            //    ListElementsArrayList.add();
-            //    adapter.notifyDataSetChanged();
-            //}
-
+        alert_button.setOnClickListener(new View.OnClickListener() {
 
             Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
             Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
 
-            public void ringtone(){
+            private void ringtone() {
                 try {
                     r.play();
                 } catch (Exception e) {
@@ -52,8 +61,15 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-
             public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,AlertActivity.class);
+                Date today = new Date();
+                DATE = today.toString();
+                i.putExtra(DATE, today.toString());
+                //startActivity(i);
+                //ListElementsArrayList.add(DATE);
+                //adapter.notifyDataSetChanged();
+
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                 builder.setCancelable(false);
                 builder.setTitle("Kid forgotten");
@@ -68,19 +84,11 @@ public class MainActivity extends AppCompatActivity {
                 builder.show();
             }
         });
-
-
     }
 
-    public void openlastalertsactivity(){
+    public void openlastalertsactivity() {
         Intent intent = new Intent(this, AlertActivity.class);
         startActivity(intent);
     }
-
-
-
-
-
-
 }
 
